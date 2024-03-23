a4b92fea543125ed2562ad9c2fb751ced612ee12
Javad Bahrami <javadev14bh@gmail.com>
2024-03-23 16:12:57 +0330

    Add auth page and send otp and check otp components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+//Pages
+import AuthPage from "./pages/AuthPage";
+
 function App() {
-  return <div>
-    <h1>پروژه دیوار</h1>
-  </div>;
+  return (
+    <div>
+      <h1>پروژه دیوار</h1>
+      <AuthPage />
+    </div>
+  );
 }
 
 export default App;
